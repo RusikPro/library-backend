53b860d04b163c5390b53badafa039af7d92030b
RusikPro <rusik.arhireev@hotmail.com>
2024-06-23 20:53:37 +0200

    Add basic models, views, urls

--- library/core/views.py
+++ library/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def health_check(request):
+    """Returns successful response."""
+    return Response({'healthy': True})
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
